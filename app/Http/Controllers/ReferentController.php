695bd264345115ed7ad8bd816e2a79fa3cec630f
fero <4gac@protonmail.com>
2019-11-12 08:41:25 +0100

    controllery pre pouzivatelov
    made by Livia

---
+++ app/Http/Controllers/ReferentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ReferentController extends Controller
+{
+    public function index()
+    {
+        return view('adminlte');
+    }
+}
