3004e1aeda33d4ea1a69681adf244873221f1911
fero <4gac@protonmail.com>
2019-11-12 08:39:27 +0100

    pridane user controllers
    made by Livia

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,19 +1,52 @@
 <?php
-
 namespace App\Http\Controllers;
-//prihodila som sem classu user aby sme nemuseli vkuse vypisovat celu cestu
-namespace App\Http\Controllers;
-use \App\User;
 use Illuminate\Http\Request;
-
+use App\User;
 class UserController extends Controller
 {
-    public function show ($id){
-        /*Ak user neexistuje hod 404 */
-      $user=User::findOrFail($id);
-      //return $user->posts;
-       return view('posts.index')
-       ->with('title', $user->name)
-       ->with('posts', $user->posts); 
+    public function showAction($id)
+    {
+        $user=User::find($id);
+        return view("users-crud/update",['user'=>$user]);
+    }
+    public function insertAction(Request $request){
+        $firstname=$request->input('firstname');
+        $lastname=$request->input('lastname');
+        $email=$request->input('Email');
+        $password=$request->input('pwd');
+        $role=$request->input('role');
+        
+        $user= new User();
+        $user->name=$firstname;
+        $user->lastname=$lastname;
+        $user->email=$email;
+        $user->password=bcrypt($password);
+        $user->role=$role;
+        $user->save();
+        return redirect()->action('UserController@showallAction');
+        
+    }
+    public function updateAction($id, Request $request){
+        $user=User::where("id","=",$id)->first();
+        $user->update(["name"=>$request->input('firstname'),
+        "lastname"=>$request->input('lastname'),
+        "email"=>$request->input('Email'),
+        "password"=>bcrypt($request->input('pwd')),
+        "role"=>$request->input('role')
+        ]);
+        return redirect()->action('UserController@showallAction');
+    }
+    public function deleteAction($id){
+     $user=User::find($id);
+     $user->delete();  
+     return redirect()->action('UserController@showallAction'); 
+    }
+    public function showallAction(){
+        $users = User::paginate(5);
+        return view("users-crud/users",['users'=>$users]);
+        }
+    
+    public function getAddUserForm(){
+        return view("users-crud/adduser");
     }
 }
