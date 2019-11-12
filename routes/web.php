02f3f8719ac9de58f3beaffefaeb9123ce19c0f6
fero <4gac@protonmail.com>
2019-11-12 08:40:53 +0100

    updatetnute routes

--- routes/web.php
+++ routes/web.php
@@ -11,19 +11,42 @@
 |
 */
 
+Route::get('/', function () {
+    return view('homepage');
+});
 
-/*Výpis článkov chceme zobraziť na homepage rovnako ako na stránke článkov. Preto požijeme 
-rovnaký controller pre / ako aj /post*/
-Route::get('/', 'PostController@index');
-Route::resource('post', 'PostController');
-
-/* Zobrazenie konkrétneho tagu a konkrétneho postu*/
-Route::get('tag/{id}', 'TagController@show');
-Route::get('user/{id}', 'UserController@show');
-
-
-/*Tieto routy si vytvorila knižnica Auth */
 Auth::routes();
+
 Route::get('/home', 'HomeController@index')->name('home');
-Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/referent', 'ReferentController@index')->name('referent')->middleware('referent');
+Route::get('/admin', 'AdminController@index')->name('admin')->middleware('admin');
+Route::get('/ucastnik', 'UcastnikController@index')->name('ucastnik')->middleware('ucastnik');
+//Route::get('/admin', 'AdminController@index');
+
+/*CRUD USEROV*/
+
+Route::get('users-crud',[
+    'as'=>'showall', 'uses'=>'UserController@showallAction'
+    ]);
+Route::get('users-crud/show/{id}',[
+'as'=>'show', 'uses'=>'UserController@showAction'
+]);
+Route::post('users-crud/insert',[
+    'as'=>'insert', 'uses'=>'UserController@insertAction'
+    ]);
+Route::post('users-crud/update/{id}',[
+        'as'=>'update', 'uses'=>'UserController@updateAction'
+        ]);
+Route::get('users-crud/add-user',[
+            'as'=>'add-user', 'uses'=>'UserController@getAddUserForm'
+            ]);
+    
+Route::get('users-crud/delete/{id}',[
+            'as'=>'delete', 'uses'=>'UserController@deleteAction'
+            ]);
+Route::get('/',[
+    'as'=>'showAll','uses' =>'PostController@showAllAction'
+]);
+Route::get('tags/showAll',[
+    'as'=>'showAll','uses' =>'TagController@showAllAction'
+]);
