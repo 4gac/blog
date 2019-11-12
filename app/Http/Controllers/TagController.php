816c2640243cdfb9e43f74611025605cc857a0c7
fero <4gac@protonmail.com>
2019-11-12 08:38:56 +0100

    pridany tag controller
    made by Majka

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -1,20 +1,10 @@
 <?php
-
 namespace App\Http\Controllers;
-//prihodila som sem classu Tag aby sme nemuseli vkuse vypisovat celu cestu
-use \App\Tag;
-use Illuminate\Http\Request;
+use App\Models\Tag;
 
-class TagController extends Controller
-{
-    public function show ($id){
-        
-      $tag=Tag::findOrFail($id);
-      return $tag->name; 
-     
-     
-   /*  return view('posts.index')
-      ->with('title', $tag->name)
-      ->with('posts', $tag->posts); */
-    }
+class TagController extends Controller{
+    public function showAllAction(){
+        $tags = Tag::all();
+        return view("tag",['tag'=>$tags]);
+        }
 }
