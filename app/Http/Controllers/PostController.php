14a3743c31af2fc272c4f3734965516eb5de58da
livi83 <livia.kelebercova@gmail.com>
2019-11-20 21:37:26 +0100

    pridanie metod pre zobrazenie postov podla tagu

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,11 +1,32 @@
 <?php
 namespace App\Http\Controllers;
 use App\Models\Post;
+use App\Models\Tag;
 
 class PostController extends Controller{
-public function showAllAction(){
+public function index(){
     $posts = Post::all();
-    return view("post",['post'=>$posts]);
-    }
+   // return view('frontend-posts.homepage-all-posts',['post'=>$posts]);
+   return view('frontend-posts.homepage-all-posts')->with('posts',$posts);    
+}
+public function show($id){
+    $posts= Post::findOrFail($id);
+    return view('frontend-posts.single-post-show')->with('posts',$posts);
+}
+public function pobyty(){
+    $posts = Post::all();
+   // return view('frontend-posts.homepage-all-posts',['post'=>$posts]);
+   return view('studijnepobyty')->with('posts',$posts);    
+}
+public function staze(){
+    $posts = Post::all();
+   // return view('frontend-posts.homepage-all-posts',['post'=>$posts]);
+   return view('pracovnestaze')->with('posts',$posts);    
+}
+public function spravy(){
+    $posts = Post::all();
+   // return view('frontend-posts.homepage-all-posts',['post'=>$posts]);
+   return view('ucastnickespravy')->with('posts',$posts);    
+}
 }
 
