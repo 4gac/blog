d7a195bcf6f5c491c0c8a52d3121c81d4bbd573b
livi83 <livia.kelebercova@gmail.com>
2019-11-20 21:50:10 +0100

    Mali sme 2 master blady. jeden som vymazala a druhy mierne upravila o bannerik a zarovnala som to

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,37 +11,39 @@
     <!-- Styles -->
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
 <body>
 
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-
-                    <a class="navbar-brand" href="#">
+                 
+                    <a class="navbar-brand" href="{{route('homepage') }}">
+                     
                         <img id="logo"src="logoukf.png" class="img-responsive" />
                     </a>
-
+                   
                   <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                     <li class="nav-item active hlavneMenuItem">
-                      <a class="nav-link" href="#">Študijné pobyty<span class="sr-only">(current)</span></a>
+                      <a class="nav-link" href="{{route('pobyty') }}">Študijné pobyty<span class="sr-only"></span></a>
                     </li>
                     <li class="nav-item hlavneMenuItem">
-                      <a class="nav-link" href="#">Pracovné stáže</a>
+                      <a class="nav-link" href="{{route('staze') }}">Pracovné stáže</a>
                     </li>
                     <li class="nav-item hlavneMenuItem">
-                      <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Účastnícke správy</a>
+                      <a class="nav-link" href="{{route('spravy') }}" tabindex="-1" aria-disabled="true">Účastnícke správy</a>
                     </li>
                     <li class="nav-item">
                             <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Kontakt</a>
                           </li>
                   </ul>
-                  <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                  </form>
+
+                </div>
+
                 </div>
               </nav>
 @yield('content')
