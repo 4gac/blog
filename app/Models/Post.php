b289dd80b4d8eb4006e612b560635ee0c2910472
livi83 <livia.kelebercova@gmail.com>
2019-11-20 21:38:51 +0100

    model obsahuje prepojenia na tabulku tagov - many to many a na tabulku userov - one to many

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,5 +8,31 @@ class Post extends Model
 {
     protected $table = 'posts';
 
-    protected $fillable = ['title','text','slug','user_id'];
+    protected $fillable = ['title','text','slug','user_id',];
+
+    /*Ak pristupim k atributu tags, post môže patriť k mnohým tagom*/
+
+
+public function tags(){
+    return $this->belongsToMany('App\Models\Tag');
+}
+
+/*Príspevok patrí userovi */
+public function user(){
+    return $this->belongsTo('App\User');
+}
+
+/* V databáze sa  časy zobrazujú pod iným formátom. Napr 2019-10-05 00:00:0
+Tento akcesor vezme čas z databázy a prehodí ho do normálneho zápisu. */
+public function getCreatedAtAttribute($value)
+{
+    return date('j M Y, G:i',strtotime($value));
+}
+/* Tento akcesor robí to, že spraví pri výpise článkov excerpt.
+  {{$post->teaser}} */
+public function getExcerptAttribute()
+{
+    return str_limit($this->text, 300);
+}
+
 }
