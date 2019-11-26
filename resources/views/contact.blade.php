a987afe9a647a4c9c85bab90c876a2d9d02d87a5
fero <4gac@protonmail.com>
2019-11-26 22:36:42 +0100

    client-side validation js

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -69,7 +69,7 @@
         </form>
 
         <div class="text-center text-md-left">
-            <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
+            <a class="btn btn-primary" onclick="validateForm()">Send</a>
         </div>
         <div class="status"></div>
     </div>
@@ -85,6 +85,37 @@
   <script type="text/javascript" src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
   <!-- MDB core JavaScript -->
   <script type="text/javascript" src="{{ asset('assets/js/mdb.min.js') }}"></script>
+  <!-- client-side validation js -->
+function validateForm() {
+    var name =  document.getElementById('name').value;
+        if (name == "") {
+            document.getElementById('status').innerHTML = "Name cannot be empty";
+        return false;
+        }
+    var email =  document.getElementById('email').value;
+        if (email == "") {
+            document.getElementById('status').innerHTML = "Email cannot be empty";
+        return false;
+        } else {
+            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(!re.test(email)){
+            document.getElementById('status').innerHTML = "Email format invalid";
+        return false;
+        }
+    }
+    var subject =  document.getElementById('subject').value;
+        if (subject == "") {
+            document.getElementById('status').innerHTML = "Subject cannot be empty";
+        return false;
+    }
+    var message =  document.getElementById('message').value;
+        if (message == "") {
+            document.getElementById('status').innerHTML = "Message cannot be empty";
+        return false;
+    }
+    document.getElementById('status').innerHTML = "Sending...";
+    document.getElementById('contact-form').submit();
+}
 
 </section>
 @endsection
