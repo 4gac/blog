dca1fcd13b2276043e8cdfe7a5471553b48f3821
fero <4gac@protonmail.com>
2019-11-26 22:30:16 +0100

    formular

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,5 +3,88 @@
 @extends('layouts/master')
 @section('title','Kontakty')
 @section('content')
-    
+<section class="mb-4">
+
+<!--Section heading-->
+<h2 class="h1-responsive font-weight-bold text-center my-4">Kontakt</h2>
+<!--Section description-->
+<p class="text-center w-responsive mx-auto mb-5">V prípade, že máte nejaké otázky, neváhajte nás kontaktovať.</p>
+
+<div class="container">
+
+    <!--Grid column-->
+    <div class="col-md-9 mb-md-0 mb-5">
+        <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+
+            <!--Grid row-->
+            <div class="row">
+
+                <!--Grid column-->
+                <div class="col-md-6">
+                    <div class="md-form mb-0">
+                        <input type="text" id="name" name="name" class="form-control">
+                        <label for="name" class="">Vaše meno</label>
+                    </div>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-md-6">
+                    <div class="md-form mb-0">
+                        <input type="text" id="email" name="email" class="form-control">
+                        <label for="email" class="">Váš email</label>
+                    </div>
+                </div>
+                <!--Grid column-->
+
+            </div>
+            <!--Grid row-->
+
+            <!--Grid row-->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="md-form mb-0">
+                        <input type="text" id="subject" name="subject" class="form-control">
+                        <label for="subject" class="">Predmet</label>
+                    </div>
+                </div>
+            </div>
+            <!--Grid row-->
+
+            <!--Grid row-->
+            <div class="row">
+
+                <!--Grid column-->
+                <div class="col-md-12">
+
+                    <div class="md-form">
+                        <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+                        <label for="message">Vaša správa</label>
+                    </div>
+
+                </div>
+            </div>
+            <!--Grid row-->
+
+        </form>
+
+        <div class="text-center text-md-left">
+            <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
+        </div>
+        <div class="status"></div>
+    </div>
+    <!--Grid column-->
+
+
+</div>
+  <!-- jQuery -->
+<script type="text/javascript" src="{{ asset('assets/js/jquery.min.js') }}"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="{{ asset('assets/js/popper.min.js') }}"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="{{ asset('assets/js/mdb.min.js') }}"></script>
+
+</section>
 @endsection
