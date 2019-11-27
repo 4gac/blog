c246f9e6886b2504b34f5199ba6398447305f2da
fero <4gac@protonmail.com>
2019-11-27 10:41:39 +0100

    pouzitie Form-u

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,117 +1,29 @@
-<?php
-?>
 @extends('layouts/master')
 @section('title','Kontakty')
 @section('content')
-<section class="mb-4">
+<?php
+         echo Form::open(array('route' => 'kontakt'));
 
-<!--Section heading-->
-<h2 class="h1-responsive font-weight-bold text-center my-4">Kontakt</h2>
-<!--Section description-->
-<p class="text-center w-responsive mx-auto mb-5">V prípade, že máte nejaké otázky, neváhajte nás kontaktovať.</p>
+            echo Form::label('Zadajte vase meno:');
+            echo Form::text('username');
+            echo '<br/>';
 
-<div class="container">
+            echo Form::label('Zadajte vas email:');
+            echo Form::text('email', 'example@gmail.com');
+            echo '<br/>';
 
-    <!--Grid column-->
-    <div class="col-md-9 mb-md-0 mb-5">
-        <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+            echo Form::label('Predmet spravy:');
+            echo Form::text('subject');
+            echo '<br/>';
 
-            <!--Grid row-->
-            <div class="row">
-
-                <!--Grid column-->
-                <div class="col-md-6">
-                    <div class="md-form mb-0">
-                        <input type="text" id="name" name="name" class="form-control">
-                        <label for="name" class="">Vaše meno</label>
-                    </div>
-                </div>
-                <!--Grid column-->
-
-                <!--Grid column-->
-                <div class="col-md-6">
-                    <div class="md-form mb-0">
-                        <input type="text" id="email" name="email" class="form-control">
-                        <label for="email" class="">Váš email</label>
-                    </div>
-                </div>
-                <!--Grid column-->
-
-            </div>
-            <!--Grid row-->
-
-            <!--Grid row-->
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="md-form mb-0">
-                        <input type="text" id="subject" name="subject" class="form-control">
-                        <label for="subject" class="">Predmet</label>
-                    </div>
-                </div>
-            </div>
-            <!--Grid row-->
-
-            <!--Grid row-->
-            <div class="row">
-
-                <!--Grid column-->
-                <div class="col-md-12">
-
-                    <div class="md-form">
-                        <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
-                        <label for="message">Vaša správa</label>
-                    </div>
-
-                </div>
-            </div>
-            <!--Grid row-->
-
-        </form>
-
-        <div class="text-center text-md-left">
-            <a class="btn btn-primary" onclick="validateForm()">Send</a>
-        </div>
-        <div class="status"></div>
-    </div>
-    <!--Grid column-->
-
-
-</div>
-  <!-- jQuery -->
-<script type="text/javascript" src="{{ asset('assets/js/jquery.min.js') }}"></script>
-  <!-- Bootstrap tooltips -->
-  <script type="text/javascript" src="{{ asset('assets/js/popper.min.js') }}"></script>
-  <!-- Bootstrap core JavaScript -->
-  <script type="text/javascript" src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-  <!-- MDB core JavaScript -->
-  <script type="text/javascript" src="{{ asset('assets/js/mdb.min.js') }}"></script>
-  <!-- client-side validation js -->
-  
-  document.getElementById('status').innerHTML = "Sending...";
-formData = {
-'name'     : $('input[name=name]').val(),
-'email'    : $('input[name=email]').val(),
-'subject'  : $('input[name=subject]').val(),
-'message'  : $('textarea[name=message]').val()
-};
-
-
-$.ajax({
-url : "mail.php",
-type: "POST",
-data : formData,
-success: function(data, textStatus, jqXHR)
-{
-
-$('#status').text(data.message);
-if (data.code) //If mail was sent successfully, reset the form.
-$('#contact-form').closest('form').find("input[type=text], textarea").val("");
-},
-error: function (jqXHR, textStatus, errorThrown)
-{
-$('#status').text(jqXHR);
-}
-});
+            echo Form::label('Vasa sprava:');   
+            echo Form::text('message');
+            echo '<br/>';
+            
+            
+            echo Form::submit('Odoslat');
+         echo Form::close();
+      ?>
 
 </section>
 @endsection
