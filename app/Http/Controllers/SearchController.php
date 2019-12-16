11bdc187ac0e3f74839982697a287aedc773b33c
Yuri1234 <juraj.benc@student.ukf.sk>
2019-12-16 12:14:13 +0100

    searchcontroller zmeny (getuni funkcia) a show-uni view

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -6,12 +6,32 @@ use Illuminate\Http\Request;
 use App\Models\UniversityModel;
 use App\Models\Post;
 use App\Models\Country;
+use Illuminate\Support\Facades\DB;
+use App\Models\UniversityPostModel;
 
 class SearchController extends Controller
 {
     public function universitySearch($id){
-		$renderData = UniversityModel::where("id", "=", $id)->first();
-		return view("search-results.show-uni")->with('renderData',$renderData);
+		$renderData = DB::table('univerzita')
+								->join('countries', 'countries.id', '=', 'univerzita.countries_id')
+								->join('mesto', 'mesto.id', '=', 'univerzita.mesto_id')
+								->select('univerzita.id', 'univerzita.nazov', 'univerzita.zmluva_od', 'univerzita.zmluva_do', 'mesto.nazov as nazovmesto', 'countries.name as nazovcountry', 'univerzita.kontaktna_osoba')
+								->where('univerzita.id', '=', $id)
+								->get()->first();;
+		$universities = DB::table('university_post')
+		->join('posts', 'posts.id', '=', 'university_post.post_id')
+		->select('university_post.id', 'posts.title', 'university_post.pocet_miest', 'university_post.studijny_odbor', 'posts.id as postid', 'posts.text')
+		->where('university_post.university_id', '=', $id)
+        ->get();
+		$postTags = DB::table('post_tag')
+								->join('posts', 'posts.id', '=', 'post_tag.post_id')
+								->join('tags', 'tags.id', '=', 'post_tag.tag_id')
+								->select('post_tag.post_id', 'post_tag.tag_id', 'posts.title', 'tags.name')
+								->get();
+
+
+
+		return view("search-results.show-uni")->with('renderData',$renderData)->with('universities',$universities)->with('postTags',$postTags);
 	}
 	public function countrySearch($id){
 		$renderData = Country::where("id", "=", $id)->first();
--- resources/views/search-results/show-uni.blade.php
+++ resources/views/search-results/show-uni.blade.php
@@ -1,11 +1,53 @@
 ﻿@extends('layouts/master')
 @section('content')
 
-{{$renderData->nazov}}
-<br>
+<div class="carousel slide" data-ride="carousel">        
+        <div class="carousel-inner" role="listbox">
+          <!-- Slide One - Set the background image for this slide in the line below -->
+          <div class="carousel-item active" style="background-image: url('http://edu.uhk.cz/mobility/wp-content/uploads/2016/08/hp-1920x698.jpg')">
+            <div class="carousel-caption d-none d-md-block">
+            <h1>{{$title or "Kontakt"}}</h1>
+            </div>
+        </div>
+</div>
 
-{{url()->current()}} <br>
+<div class="container news-post">
+<h2>{{$renderData->nazov}}</h2>
+<h4>{{$renderData->nazovmesto}} - {{$renderData->nazovcountry}}</h4>
+<p> Zmluva s univerzitou {{$renderData->nazov}} od: <b>{{$renderData->zmluva_od}}</b> - <b>{{$renderData->zmluva_do}}</b> </p>
+<p>Kontaktná osoba: {{$renderData->kontaktna_osoba}}</p>
 
-<?php echo "http://" . $_SERVER['SERVER_NAME']?>
+<hr align="left" width="20%">
+<div class="unitab">
+        <h2>Výzvy v ponuke</h2>
+        @forelse($universities as $uni)
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Typ výzvy</th>
+                <th scope="col">Názov výzvy</th>
+                <th scope="col">Študijný odbor</th>
+                <th scope="col">Počet miest</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+				@foreach($postTags as $postTag) @if($postTag->title == $uni->title) {{$postTag->name}} @endif @endforeach
+				</td>
+                <td>{{$uni->title}}</td>
+                <td>{{$uni->studijny_odbor}}</td>
+                <td>{{$uni->pocet_miest}}</td
+              </tr>
+            </tbody>
+          </table>
+        </div>
+    @empty
+            <p>nič, man</p>
+            @endforelse
+</div>
+</div>
+
+
 
 @endsection
